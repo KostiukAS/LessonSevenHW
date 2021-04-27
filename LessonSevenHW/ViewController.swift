3305bba63af37fa855a2947b06fd2b4392353d04
KostiukAS <kostiukandrey2005@gmail.com>
2021-04-27 19:18:55 +0300

    Initial Commit

---
+++ LessonSevenHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonSevenHW
+//
+//  Created by admin on 27.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
